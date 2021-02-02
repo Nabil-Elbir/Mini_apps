b9041c28878b58f0ec85684dbd080b40c65b141a
Nabil-Elbir <nabil.elbir@gmail.com>
2021-02-02 16:27:10 +0100

    Ensure the clicked square is empty

--- Tic_Tac_Toe/app.js
+++ Tic_Tac_Toe/app.js
@@ -4,15 +4,20 @@
 var cells = document.querySelectorAll(".square");
 for (var cell of cells) {
     cell.addEventListener('click', marker)
-}
+};
 
 var counter = 0;
+
 function marker() {
-    counter += 1;
-    if (counter % 2 === 0) {
-        this.innerHTML = "O";
-    } else {
-        this.innerHTML = "X";
+    if (this.innerHTML.length === 0) {     // Ensure the clicked square is empty. 
+        counter += 1;
+        if (counter % 2 === 0) {
+            this.innerHTML = "O";
+        } else {                           // Swap the state in each play.
+            this.innerHTML = "X";
+        }
     }
-}
+};
+
+
 
